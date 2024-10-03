c8375620ca651d5e4a3a13ed3790c582d2208af3
UNAPIMD <ExpIgn@gmail.com>
2024-10-03 21:05:33 +0300

    Add SimpleTask: Container With Most Water

---
+++ SimpleTask/SimpleTask/SimpleTask.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SimpleTask
+{
+    public static class SimpleTask
+    {
+        /// <summary>
+        /// Container With Most Water
+        /// </summary>
+        /// <param name="height"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static int MaxArea(int[] height)
+        {
+            if (height == null) throw new ArgumentNullException("height is null");
+
+            int left = 0; //Левая граница ёмкости
+            int right = height.Length - 1; //Правая граница ёмкости
+            int V = 0; //Искомый объём
+            while(left < right)
+            {
+                V = Math.Max(V, Math.Min(height[left], height[right]) * (right - left));
+                //Будем двигать границу с наименьшей высотой, иначе, объём жидкости будет только уменьшаться
+                if (height[left] < height[right]) left++;
+                else right--;
+            }
+            return V;
+        }
+    }
+}
---
+++ SimpleTask/SimpleTask/main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SimpleTask;
+
+
+namespace SimpleTask
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine(SimpleTask.MaxArea(new int[] { 1, 2, 0, 5, 1, 4 }));
+        } 
+    }
+}
